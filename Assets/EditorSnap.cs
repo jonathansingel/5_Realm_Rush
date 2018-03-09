e2c4e5b6d6cd5b42de9c1901344e5dd7f0446470
JonathanSingel <jonathansingel@gmail.com>
2018-03-09 12:23:28 -0500

    10f snapped grid.

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / 10f) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / 10f) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+	}
+}
