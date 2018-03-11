f4854c2347e73c88ce85110aa55f84ed7214b281
JonathanSingel <jonathansingel@gmail.com>
2018-03-11 12:37:22 -0400

    end of course?

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns = 3f;
+    [SerializeField] GameObject enemyToSpawn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(SpawnEnemy());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    IEnumerator SpawnEnemy()
+    {
+        while (true) //forever
+        {
+            print("spawning");
+
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+}
